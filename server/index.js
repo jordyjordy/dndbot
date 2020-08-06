ca7adc82060670b8d3e20605db84312ea7fd963f
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2020-08-06 14:07:30 +0200

    server 'or' port fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.DATABASE_URL).then(() => {
     console.log("Connected to database!")
 
 })
-const port = process.env.PORT | 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
