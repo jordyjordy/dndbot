a64590aacf46d8590406ce4d9d97f76b23529d13
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2020-08-08 12:52:29 +0200

    update during game timing

--- bot/commands/dnd.js
+++ bot/commands/dnd.js
@@ -3,7 +3,6 @@ module.exports= {
     name:'?dnd',
     description:'Gives time until the next session!',
     execute(msg,args) {
-        console.log("HI!")
         var now = new Date();
         const day = now.getDay()
         let diff = process.env.WEEKDAY - day
@@ -11,14 +10,18 @@ module.exports= {
             diff += 7
         }
         const dndDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + diff,process.env.HOUR,process.env.MINUTE,0)
+        console.log(msg.author.username)
         let difference = dndDay.getTime() - now.getTime()
         let days = Math.floor((difference / (1000*60*60*24)) % 365)
         let hours = Math.floor((difference / (1000 * 60 * 60)) % 24)
         let minutes = Math.floor((difference / (1000 * 60)) % 60)
-        console.log("hey")
         switch (diff) {
             case 0:
-                msg.channel.send(`OMG OMG OMG AAAAAAAAAAH DND IS TODAY!\nONLY ${hours} HOURS AND ${minutes} MINUTES REMAINING!`)
+                if(difference <= 0) {
+                    msg.channel.send(`DND IS NOW, WHAT ARE YOU WAITING FOR GO PLAY!`)
+                } else {
+                    msg.channel.send(`OMG OMG OMG AAAAAAAAAAH DND IS TODAY!\nONLY ${hours} HOURS AND ${minutes} MINUTES REMAINING!`)
+                }
                 msg.channel.send(`WEEEEEEEEEEEEEEEEEEEEEEEEEEEE`)
                 break;
             case 1:
@@ -33,11 +36,12 @@ module.exports= {
                 msg.channel.send(`DnD is in ${days} days, ${hours} hours and ${minutes} minutes!`)
                 break;
             default: 
-                console.log(diff)
-                console.log("huh")
                 //Theres not that many days in the week, we shouldnt get here
                 break;
 
+        }
+        if(msg.author.username == 'itsjustjohn'){
+            msg.author.send('John John John, you of all people should really know when the next session is happening!')
         }
     },
 };
