a131bf622486f14859bd800580eba6105d7a10ce
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2021-07-14 15:14:52 +0200

    log result

--- client/src/services/apiservice.ts
+++ client/src/services/apiservice.ts
@@ -9,6 +9,7 @@ export default {
         const url = ip + "/item/list"
         try {
             const result = await axios.get(url)
+            console.log(result)
             return result.data
         } catch (err) {
             return []
