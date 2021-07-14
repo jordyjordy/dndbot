a887ee2ab4fc47186a2134431c5e15d52bc93833
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2021-07-14 16:27:22 +0200

    resolve route and clear console logs

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,15 +1,11 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue'
-const app = createApp({})
+
 const routes = [
   {
     path: '/list',
     name: 'List',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "list" */ '../views/List.vue')
+    component: () => import('../views/List.vue')
   },
   {
     path: '/edit/:id',
@@ -33,21 +29,15 @@ const routes = [
     component: () => import('../views/Schedule.vue')
   },
   {
-    path: '/*',
-    name: "leftover",
-    component: app.component("page-not-found", {
-      template: "",
-      created: function () {
-        this.$router.push("/list")
-      }
-    })
-
+    path: '/:pathMatch(.*)*',
+    redirect: '/list',
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: routes,
+  linkActiveClass: 'active'
 })
 
 export default router
