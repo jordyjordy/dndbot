e4f43fde45f56a8c478c813372ff60ce638a7486
jordy <jordyvandertang@hotmail.com>
2021-09-15 16:29:40 +0200

    allow specifying addsong index

--- bot/commands/addsong.js
+++ bot/commands/addsong.js
@@ -1,10 +1,16 @@
 connectionManager = require('../connectionManager')
 module.exports = {
     name: '?addsong',
-    description: 'Adds a song to the music queue',
+    description: 'Adds a song to the music queue, optional param to indicate the index',
     async execute(msg, args) {
-        if(!await connectionManager.queueSong(args[0])) {
-            msg.channel.send("something went wrong, possibly you entered a bad url.")
+        if(args.length > 1) {
+            if(!await connectionManager.queueSong(args[0],args[1])) {
+                msg.channel.send("something went wrong, possibly you entered a bad url.")
+            }
+        } else {
+            if(!await connectionManager.queueSong(args[0])) {
+                msg.channel.send("something went wrong, possibly you entered a bad url.")
+            }
         }
     }
 }
