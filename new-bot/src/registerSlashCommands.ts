9e79450c2c6b94827598092f13b5f29fd037a203
jordy <jordyvandertang@hotmail.com>
2022-01-15 22:57:14 +0100

    no longer clear old commands

--- new-bot/src/registerSlashCommands.ts
+++ new-bot/src/registerSlashCommands.ts
@@ -22,17 +22,17 @@ async function registerSlashCommands(servers:string[]):Promise<void> {
 
 const refreshSlashCommmands = async (server: string, rest: REST, commands) => {
     try{
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const oldCommands: Array<any> | any = await rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID, server));
-        oldCommands.forEach(async (command) => {
-            const deleteUrl: `/${string}` = `${Routes.applicationGuildCommands(process.env.CLIENT_ID, server)}/${command.id}`;
-            try{ 
-                await rest.delete(deleteUrl);
-            } catch(err) {
-                console.log('could not delete command')
-                // do nothing
-            }
-        })
+        // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        // const oldCommands: Array<any> | any = await rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID, server));
+        // oldCommands.forEach(async (command) => {
+        //     const deleteUrl: `/${string}` = `${Routes.applicationGuildCommands(process.env.CLIENT_ID, server)}/${command.id}`;
+        //     try{ 
+        //         await rest.delete(deleteUrl);
+        //     } catch(err) {
+        //         console.log('could not delete command')
+        //         // do nothing
+        //     }
+        // })
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, server),
             { body: commands }
